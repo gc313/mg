ba9eec7270b38280c2f61311607a0b080ca2291d
gc313 <gc313@foxmail.com>
2018-03-29 14:24:21 +0800

    fix one little bug

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ import Module.initialization as ini
 import Module.handler as hd
 import Module.select as se
 import Module.config as con
-
+'''
 def Now():
 	return str(datetime.now())[:19]
+'''
 
 if __name__ == "__main__":
-'''
+	'''
 	try:
 		con.logger.info('程序运行')
 		while 1:
@@ -25,11 +26,11 @@ if __name__ == "__main__":
 	except Exception as e:
 		con.logger.info("程序报错%s" % (e))
 	finally:
-		con.logger.info('程序结束运行')
-'''
-    #ini.init_table() #初始化表格
-    #ini.init_list() #采集地理数据
-    hd.get_orders_data() #采集订单数据
-    #se.find_tables(con.order_db, 1234)
-    se.out_put() #输出
-    #se.route('30000002', '30001688')
+		con.logger.info("程序结束运行")
+	'''
+	#ini.init_table() #初始化表格
+	#ini.init_list() #采集地理数据
+	hd.get_orders_data() #采集订单数据
+	#se.find_tables(con.order_db, 1234)
+	se.out_put() #输出
+	#se.route('30000002', '30001688')
